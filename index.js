d9098a09130b098f4062ecec9490b09159e9fa66
NSD <nsd24technology@gmail.com>
2020-07-20 10:44:38 +0900

    add emitting events function

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
     });
 });
 
