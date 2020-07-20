0594262e72d52a1484823222a0114f2140fc78aa
NSD <nsd24technology@gmail.com>
2020-07-20 10:31:28 +0900

    prepare socket.io

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
 http.listen(3000, () => {
