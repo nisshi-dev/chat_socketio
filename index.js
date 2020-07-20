7cd28d2502d386dbf455cbf5f2474242a4f9e18b
NSD <nsd24technology@gmail.com>
2020-07-20 10:32:27 +0900

    dicsonect detection on socket.io

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
